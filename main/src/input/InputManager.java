4636ddb3e878839b7eed855a541369e380429ab2
BennTay <benntayguobin@gmail.com>
2019-01-19 20:30:33 +0800

    Add InputManager

--- main/src/input/InputManager.java
+++ main/src/input/InputManager.java
@@ -1,12 +1,46 @@
 package input;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Stores the movement states (true or false) of each movement direction (up, down, left, right)
+ * for each of the four players.
+ */
 public class InputManager {
-    public GameKeyListener getInput(String key) {
-        return GameKeyListener.getSingleton();
+
+    private static InputManager singleton;
+    private ArrayList<HashMap<String, Boolean>> playerInputs;
+
+    private InputManager() {
+        playerInputs = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            HashMap<String, Boolean> hmap = new HashMap<>();
+            hmap.put("up", false);
+            hmap.put("down", false);
+            hmap.put("left", false);
+            hmap.put("right", false);
+            playerInputs.add(hmap);
+        }
     }
 
-    public void updateInputs(GameKeyListener gameKeyListener) {
+    public static InputManager getSingleton() {
+        if (singleton == null) {
+            singleton = new InputManager();
+        }
+        return singleton;
+    }
 
+    public ArrayList<HashMap<String, Boolean>> getPlayerInputs() {
+        return playerInputs;
+    }
+
+    public void setPlayerMovement(int playerNum, String movementDirection, boolean movementState) {
+        playerInputs.get(playerNum).replace(movementDirection, movementState);
+    }
+
+    public GameKeyListener getInput(String key) {
+        return GameKeyListener.getSingleton();
     }
 
 }
