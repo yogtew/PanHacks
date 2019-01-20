9e18b987dd1125164b1e6f7e571f4f4d90d2517b
BennTay <benntayguobin@gmail.com>
2019-01-20 11:56:35 +0800

    Change to arrow keys, add space

--- main/src/input/InputManager.java
+++ main/src/input/InputManager.java
@@ -1,5 +1,6 @@
 package input;
 
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -51,6 +52,7 @@ public class InputManager {
         hmap.put("down", false);
         hmap.put("left", false);
         hmap.put("right", false);
+        hmap.put("space", false);
         getSingleton().playerInputs.put(id, hmap);
     }
 
@@ -68,19 +70,19 @@ public class InputManager {
 
     @Subscribe
     public void updateKeyPress(KeyPressedEvent e) {
-        char c = e.getKeyEvent().getKeyChar();
+        int code = e.getKeyEvent().getKeyCode();
 
-        switch (c) {
-            case 'w':
+        switch (code) {
+            case KeyEvent.VK_UP:
                 playerInputs.get(1).put("up", true);
                 break;
-            case 'a':
+            case KeyEvent.VK_LEFT:
                 playerInputs.get(1).put("left", true);
                 break;
-            case 's':
+            case KeyEvent.VK_DOWN:
                 playerInputs.get(1).put("down", true);
                 break;
-            case 'd':
+            case KeyEvent.VK_RIGHT:
                 playerInputs.get(1).put("right", true);
                 break;
         }
@@ -88,18 +90,19 @@ public class InputManager {
 
     @Subscribe
     public void updateKeyRelease(KeyReleasedEvent e) {
-        char c = e.getKeyEvent().getKeyChar();
-        switch (c) {
-            case 'w':
+        int code = e.getKeyEvent().getKeyCode();
+
+        switch (code) {
+            case KeyEvent.VK_UP:
                 playerInputs.get(1).put("up", false);
                 break;
-            case 'a':
+            case KeyEvent.VK_LEFT:
                 playerInputs.get(1).put("left", false);
                 break;
-            case 's':
+            case KeyEvent.VK_DOWN:
                 playerInputs.get(1).put("down", false);
                 break;
-            case 'd':
+            case KeyEvent.VK_RIGHT:
                 playerInputs.get(1).put("right", false);
                 break;
         }
